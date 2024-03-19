8999320099223a067e70a4d751deb7e7737bd14a
Novice <novice@Xcode-Air.local>
2024-03-19 14:09:32 +0200

    lastupdate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
    <div>
     
     <div className="QRbox rounded-2xl	bg-white	justify-center block p-5 mt-10 space-y-8 shadow-2xl">
-        <Image src={qr} alt="image" width='400' height={400}/>
+        <Image src={qr} alt="image" className="rounded-2xl"/>
 
       <h2 className="font-extrabold	text-center	text-2xl	p-2">Improve your front-end skills by building projects</h2>
       <h2 className="text-center p-2">Scan the QR code to visit Frontend mentor and take your coding skills to the next level</h2>
